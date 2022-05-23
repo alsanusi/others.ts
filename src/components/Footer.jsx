da29f683863fe198b5434f73fe81fca6d14d0203
alsanusi <malkautsars@gmail.com>
2022-05-23 14:12:39 +0800

    feat: footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="white" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Others.ts
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+export default function Footer() {
+  return (
+    <Box sx={{ bgcolor: "#1B1E23", p: 3 }} component="footer">
+      <Copyright />
+    </Box>
+  );
+}
