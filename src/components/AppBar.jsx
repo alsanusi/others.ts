62ef0380f494ba74753a34a74434d341c71e6c69
alsanusi <malkautsars@gmail.com>
2022-05-23 14:20:30 +0800

    feat: appbar component

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+
+// Assets
+import OthersLogo from "../assets/img/others-logo.png";
+
+export default function NavigationBar() {
+  return (
+    <AppBar position="relative" sx={{ bgcolor: "#1B1E23" }}>
+      <Toolbar>
+        <img src={OthersLogo} alt="Others.ts" height="65" width="66" />
+      </Toolbar>
+    </AppBar>
+  );
+}
