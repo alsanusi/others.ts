b275e045ab6ebda750ba3d46e62bcef1710faf3b
alsanusi <malkautsars@gmail.com>
2022-05-23 14:58:56 +0800

    feat: blog data

---
+++ src/components/BlogCard.jsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+
+export default function BlogCard({ ...props }) {
+  return (
+    <Card
+      sx={{
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        boxShadow: "none",
+      }}
+    >
+      <CardMedia
+        component="img"
+        height="200"
+        image="https://source.unsplash.com/random"
+        alt="random"
+      />
+      <CardContent sx={{ flexGrow: 1, padding: 0, paddingTop: 1.5 }}>
+        <Grid container sx={{ marginBottom: 2 }}>
+          <Grid item xs={6} md={8}>
+            <div style={{ display: "flex", alignSelf: "center" }}>
+              <CalendarTodayIcon size="small" sx={{ mr: 1 }} />
+              <Typography variant="body2" sx={{ alignSelf: "center" }}>
+                {props.date}
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item xs={6} md={4}>
+            <Typography
+              gutterBottom
+              variant="body2"
+              sx={{ textAlign: "right" }}
+            >
+              {props.writer}
+            </Typography>
+          </Grid>
+        </Grid>
+        <Typography gutterBottom variant="h5" fontWeight="bold">
+          {props.title}
+        </Typography>
+        <Typography variant="subtitle2">{props.description}</Typography>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ src/utils/indexData.js
@@ -0,0 +1,44 @@
+export const indexData = [
+    {
+        id: 1,
+        date: '28 Maret 2022',
+        writer: 'Admin',
+        title: 'Memilih foto yang cocok untuk desain website',
+        description: 'Tips memilih foto yang cocok untuk desain website dan bagaimana cara menempatkannya dengan tepat'
+    },
+    {
+        id: 2,
+        date: '28 Maret 2022',
+        writer: 'Admin',
+        title: 'Membuat desain header yang profesional',
+        description: 'Tips memilih foto yang cocok untuk desain website dan bagaimana cara menempatkannya dengan tepat'
+    },
+    {
+        id: 3,
+        date: '28 Maret 2022',
+        writer: 'Admin',
+        title: 'Cara memakai inspirasi dari referensi',
+        description: 'Tips memilih foto yang cocok untuk desain website dan bagaimana cara menempatkannya dengan tepat'
+    },
+    {
+        id: 4,
+        date: '28 Maret 2022',
+        writer: 'Admin',
+        title: 'Sebaiknya belajar UI Design mulai dari mana?',
+        description: 'Tips memilih foto yang cocok untuk desain website dan bagaimana cara menempatkannya dengan tepat'
+    },
+    {
+        id: 5,
+        date: '28 Maret 2022',
+        writer: 'Admin',
+        title: 'Membuat desain header yang profesional',
+        description: 'Tips memilih foto yang cocok untuk desain website dan bagaimana cara menempatkannya dengan tepat'
+    },
+    {
+        id: 6,
+        date: '28 Maret 2022',
+        writer: 'Admin',
+        title: 'Cara memakai inspirasi dari referensi',
+        description: 'Tips memilih foto yang cocok untuk desain website dan bagaimana cara menempatkannya dengan tepat'
+    }
+]
